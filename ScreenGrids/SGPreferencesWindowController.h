69667a5d3a3ec7f2af24e9e969b3d8456bcbe6c2
albert_lee <lijinbo0122@gmail.com>
2012-05-12 23:40:34 +0800

    UI setup

--- ScreenGrids/SGPreferencesWindowController.h
+++ ScreenGrids/SGPreferencesWindowController.h
@@ -8,11 +8,38 @@
 
 #import <Cocoa/Cocoa.h>
 #import "DBPrefsWindowController.h"
-@interface SGPreferencesWindowController : DBPrefsWindowController
+@interface SGPreferencesWindowController : DBPrefsWindowController<NSTableViewDataSource,NSTableViewDelegate>
 {  
   DBPrefsWindowController *preferenceControl;
   IBOutlet NSView *generalView;
   IBOutlet NSView *appearanceView;
+  IBOutlet NSView *arragementView;
   IBOutlet NSView *advancedView;
+  
+  IBOutlet NSButton *checkBoxButton;
+  
+  IBOutlet NSComboBox *gridFrameStyleComboBox;
+  IBOutlet NSComboBox *gridBackgroundStyleComboBox;
+  IBOutlet NSComboBox *gridArrangementStyleComboBox;
+  
+  IBOutlet NSButton *solidColorBackgroundButton;
+  IBOutlet NSButton *customImageBackgroundButton;
+  IBOutlet NSButton *frameColorButton;
+  
+  IBOutlet NSTableView *themeTableView;
+  IBOutlet NSView *themeStandardView;
+  
+  
+  IBOutlet NSView *customArrangementView;
+  IBOutlet NSComboBox *verticalNumComboBox;
+  IBOutlet NSComboBox *verticalProportionFirst;
+  IBOutlet NSComboBox *verticalProportionSecond;
+  
+  IBOutlet NSComboBox *horizontalNumComboBox;
+  IBOutlet NSComboBox *horizontalProportionFirst;
+  IBOutlet NSComboBox *horizontalProportionSecond;
 }
+
+- (void)arrangementTabInit;
+- (void)appearanceTabInit;
 @end
--- ScreenGrids/SGPreferencesWindowController.m
+++ ScreenGrids/SGPreferencesWindowController.m
@@ -26,6 +26,10 @@
           label:@"Appearance" 
           image:[NSImage imageNamed:@"Appearance"]
      identifier:@"prefAppearanceIdentifier"];
+  [self addView:arragementView
+          label:@"Arrangement" 
+          image:[NSImage imageNamed:@"Arrangement"]
+     identifier:@"prefArrangementIdentifier"];
   [self addView:advancedView
           label:@"Advanced" 
           image:[NSImage imageNamed:@"Advanced"]
@@ -34,8 +38,49 @@
   [self setCrossFade:YES];
   [self.window setTitle:@"ScreenGrids Preferences"];
   
-
+  
+  [self appearanceTabInit];
+  [self arrangementTabInit];
 }
 
 
+- (void)arrangementTabInit{
+  [gridArrangementStyleComboBox selectItemAtIndex:0];
+  
+  NSImageView *imageView = [[NSImageView alloc] initWithFrame:NSMakeRect(28, 95, 343, 212)];
+  imageView.image = [NSImage imageNamed:@"customArrangement.png"];
+  imageView.imageScaling = NSScaleToFit;
+  
+  [customArrangementView addSubview:imageView];
+}
+
+- (void)appearanceTabInit{
+  [gridFrameStyleComboBox selectItemAtIndex:0];
+  [gridBackgroundStyleComboBox selectItemAtIndex:0];
+  
+  NSImageView *imageView = [[NSImageView alloc] initWithFrame:NSMakeRect(0, 0, 240, 150)];
+  imageView.image = [NSImage imageNamed:@"template_1.png"];
+  imageView.imageScaling = NSScaleToFit;
+  
+  [themeStandardView addSubview:imageView];
+}
+#pragma mark -Table View Datasourse
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView{
+  return 6;
+}
+
+- (id)tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row{
+  if (themeTableView ==tableView) {
+    
+  }
+  return nil;
+}
+
+#pragma mark -Table View Delegate
+- (NSView*)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row{
+  NSImageView *imageView = [[NSImageView alloc] initWithFrame:NSMakeRect(0, 0, 109, 100)];
+  imageView.image = [NSImage imageNamed:[NSString stringWithFormat:@"template_thumb_%d",row+1]];
+  imageView.imageScaling = NSScaleToFit;
+  return imageView;
+}
 @end
